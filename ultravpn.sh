f2c105024b29e9c73567fe740a3e3de5f1c6f6f0
UltraVPN <Josh.tesol@gmail.com>
2016-04-24 21:47:17 -0700

    Update ultravpn.sh

--- ultravpn.sh
+++ ultravpn.sh
@@ -3,6 +3,7 @@ apt-get update && apt-get upgrade
 apt-get install lynx -y
 wget http://www.softether-download.com/files/softether/v4.21-9613-beta-2016.04.24-tree/Linux/SoftEther_VPN_Server/64bit_-_Intel_x64_or_AMD64/softether-vpnserver-v4.21-9613-beta-2016.04.24-linux-x64-64bit.tar.gz
 tar xzvf softether-vpnserver-v4.21-9613-beta-2016.04.24-linux-x64-64bit.tar.gz
+
 apt-get install build-essential -y
 cd vpnserver
 make
